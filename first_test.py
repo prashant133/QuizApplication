5098bb8adb5cc5991edd23ceef32e1e18fee82a4
prashan133 <dsrprasant@gmail.com>
2021-10-04 15:23:37 +0545

    Testing

--- first_test.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-@pytest.mark.parametrize("email,password",[("dsrprasant@gmail.com,123"),("xyq@gmail.com,qwe")])
-def test_method(email,password):
-    assert email == "dsrprasant@gmail.com"
--- second_test.py
+++
@@ -1,9 +0,0 @@
-
-
-
-import pytest
-
-@pytest.mark.parametrize("x,y,z",[(10,20,200),(20,40,200)])
-
-def test_method(x,y,z):
-    assert x * y == z
